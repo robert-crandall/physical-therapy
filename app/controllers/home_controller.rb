addbe7f6111b677065998a1bfbec520aac7561e6
Robert Crandall <robert.crandall@gmail.com>
2025-01-02 11:20:36 -0800

    Add exercies to homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @categories = Category.where(enabled: true).order(:order)
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
   has_many :exercises
+
+  def next_exercise
+    exercises.first # Later can add logic for progression
+  end
 end
