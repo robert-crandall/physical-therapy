8011122b8ea861aaae8c14e2cd43565fe9672b8f
Robert Crandall <robert.crandall@gmail.com>
2025-01-02 11:21:47 -0800

    Enable pwa

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "home#index"
-  get 'settings', to: 'settings#index'
+  get "settings", to: "settings#index"
 
   resources :exercises
   resources :categories
@@ -11,8 +11,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
   # root "posts#index"
