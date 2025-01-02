2d89dcd90f4f2bbb2a8beca5f108639590c2fc37
Robert Crandall <robert.crandall@gmail.com>
2025-01-02 12:01:06 -0800

    Better history

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,15 +1,26 @@
 class Category < ApplicationRecord
   has_many :exercises
 
-  def next_exercise
+  def next_exercises
     exercises
-      .where.not(id: exercise_history.select(:exercise_id))
-      .first || exercises.first # If all completed, start over
+      .left_joins(:exercise_histories)
+      .select("exercises.*, MAX(exercise_histories.created_at) as last_completed")
+      .group("exercises.id")
+      .order(Arel.sql("last_completed ASC NULLS FIRST"))
+      .limit(remaining_quality)
   end
 
   private
 
+  def remaining_quality
+    quantity - recently_completed_exercises.count
+  end
+
+  def recently_completed_exercises
+    exercise_history.where("created_at > ?", 2.hours.ago).limit(quantity)
+  end
+
   def exercise_history
-    ExerciseHistory.where(exercise: self.exercises)
+    ExerciseHistory.where(exercise: self.exercises).order(created_at: :desc)
   end
 end
--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,4 +1,4 @@
 class Exercise < ApplicationRecord
   belongs_to :category
-  has_many :exercise_history
+  has_many :exercise_histories
 end
