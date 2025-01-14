1b2fc123e6d366b9c30c9a0309e6f2da73c02e93
Robert Crandall <robert.crandall@gmail.com>
2025-01-14 08:14:46 -0800

    Duration validation

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -3,4 +3,18 @@ class Exercise < ApplicationRecord
 
   belongs_to :category
   has_many :exercise_histories, dependent: :destroy
+
+  validate :duration_long_enough
+
+  private
+
+  def duration_long_enough
+    if reps.present? && duration.present?
+      errors.add(:base, "Cannot have both reps and duration. Choose reps if duration is less than 10 seconds.")
+    end
+
+    if duration.to_i < 10
+      errors.add(:base, "Duration must be at least 10 seconds. Choose reps if duration is less than 10 seconds.")
+    end
+  end
 end
